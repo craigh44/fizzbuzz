8396872811b67858ed09dfaeeab2d9f637d70274
Craig Horsborough <craighorsborough@gmail.com>
2014-09-22 20:10:06 +0100

    Added first few tests

---
+++ spec/fizzbuzz_spec.rb
@@ -0,0 +1,36 @@
+require 'fizzbuzz'
+
+describe 'FizzBuzz' do 
+
+
+
+	context 'knows that a number is divisible by' do 
+
+		it '3' do 
+			expect(is_divisible_by_three?(3)).to be true
+		end
+
+		it "5" do 
+			expect(is_divisible_by_five?(5)).to be true
+		end
+		it '15' do 
+			expect(is_divisible_by_fifteen?(15)).to be true
+		end
+	end
+		
+
+
+
+	context 'knows that a number is not divisible by' do 
+		it '3' do 
+			expect(is_divisible_by_three?(1)).not_to be true
+		end
+		it '5' do
+			expect(is_divisible_by_five?(1)).not_to be true
+		end
+		it '15' do 
+			expect(is_divisible_by_fifteen?(1)).not_to be true
+		end
+	end
+
+end
