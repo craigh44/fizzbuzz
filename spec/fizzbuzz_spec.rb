44bde66bc4cc9d4ee0801ed149739b55f5661522
Craig Horsborough <craighorsborough@gmail.com>
2014-09-26 21:49:32 +0100

    Finished FizzBuzz

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -33,4 +33,20 @@ describe 'FizzBuzz' do
 		end
 	end
 
+	context "while playing the game it returns" do
+		it "the number" do
+			expect(fizzbuzz(1)).to eq 1
+	end
+		it "Fizz" do 
+			expect(fizzbuzz(3)).to eq "Fizz"
+		end
+
+		it "Buzz" do 
+			expect(fizzbuzz(5)).to eq "Buzz"
+		end 
+
+		it "FizzBuzz" do 
+			expect(fizzbuzz(15)).to eq "FizzBuzz"
+		end
+	end
 end
